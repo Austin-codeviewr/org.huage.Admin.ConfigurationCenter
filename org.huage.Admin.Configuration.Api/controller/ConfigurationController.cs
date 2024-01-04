d688e5e26d8c3daed19125f4ed060ac349cc82be
jing <jingxinpeng@kingagroot.com>
2024-01-04 14:06:47 +0800

    Initial commit

---
+++ org.huage.Admin.Configuration.Api/controller/ConfigurationController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace org.huage.Admin.Configuration.Api.controller;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class ConfigurationController : ControllerBase
+{
+    
+}
