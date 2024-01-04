c464006229e54ffe88bb199d2bc5b41d41a1896b
jing <jingxinpeng@kingagroot.com>
2024-01-04 14:08:18 +0800

    add the document.

--- org.huage.Admin.Configuration.Api/controller/ConfigurationController.cs
+++ org.huage.Admin.Configuration.Api/controller/ConfigurationController.cs
@@ -2,6 +2,10 @@
 
 namespace org.huage.Admin.Configuration.Api.controller;
 
+/**
+ * 希望可以根据动态路由携带的org_id去查询不同application的配置值。
+ */
+
 [ApiController]
 [Route("api/[controller]/[action]")]
 public class ConfigurationController : ControllerBase
